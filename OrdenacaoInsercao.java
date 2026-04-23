c71a5a459f4e7edb1fe7f3fd9972cbae480c4492
Izabelanaves <navesizabela@gmail.com>
2026-04-23 15:12:26 -0300

    adiciona questoes 3 e 4

---
+++ OrdenacaoInsercao.java
@@ -0,0 +1,250 @@
+import java.io.File;
+import java.io.FileWriter; // Para gravar o arquivo de log
+import java.io.IOException;
+import java.util.Scanner;
+
+class Restaurante {
+	private int id;
+	private String nome;
+	private String cidade;
+	private int capacidade;
+	private double avaliacao;
+	private String[] tiposCozinha;
+	private int faixaPreco;
+	private Hora horarioAbertura;
+	private Hora horarioFechamento;
+	private Data dataAbertura;
+	private boolean aberto;
+
+	public Restaurante(int id, String nome, String cidade, int capacidade, double avaliacao, String[] tiposCozinha, int faixaPreco, Hora horarioAbertura, Hora horarioFechamento, Data dataAbertura,
+					   boolean aberto)
+	{
+		this.id = id;
+		this.nome = nome;
+		this.cidade = cidade;
+		this.capacidade = capacidade;
+		this.avaliacao = avaliacao;
+		this.tiposCozinha = tiposCozinha;
+		this.faixaPreco = faixaPreco;
+		this.horarioAbertura = horarioAbertura;
+		this.horarioFechamento = horarioFechamento;
+		this.dataAbertura = dataAbertura;
+		this.aberto = aberto;
+	}
+
+	public int getId()
+	{
+		return this.id;
+	}
+
+	public String getCidade()
+	{
+		return this.cidade;
+	}
+
+	public static Restaurante parseRestaurante(String s)
+	{
+		try (Scanner sc = new Scanner(s)) {
+			sc.useDelimiter(",");
+
+			int id = sc.nextInt();
+			String nome = sc.next();
+			String cidade = sc.next();
+			int capacidade = sc.nextInt();
+			double avaliacao = sc.nextDouble();
+			String[] tiposCozinha = parseTiposCozinha(sc.next());
+			int faixaPreco = sc.next().length();
+
+			sc.useDelimiter("-");
+			Hora horarioAbertura = Hora.parseHora(sc.next());
+			sc.useDelimiter(",");
+			Hora horarioFechamento = Hora.parseHora(sc.next());
+
+			Data dataAbertura = Data.parseData(sc.next());
+
+			boolean aberto = (sc.next().compareTo("true") == 0);
+
+			return new Restaurante(id, nome, cidade, capacidade, avaliacao, tiposCozinha, faixaPreco, horarioAbertura, horarioFechamento, dataAbertura, aberto);
+		}
+	}
+
+	private static String[] parseTiposCozinha(String s)
+	{
+		int count = 1;
+		for (int i = 0; i < s.length(); ++i)
+			if (s.charAt(i) == ';')
+				++count;
+
+		String[] tipos = new String[count];
+		try (Scanner sc = new Scanner(s)) {
+			sc.useDelimiter(";");
+			int i = 0;
+			while (sc.hasNext())
+				tipos[i++] = sc.next();
+		}
+		return tipos;
+	}
+
+	public String formatar()
+	{
+		String tiposFormatados = "";
+		for (int i = 0; i < tiposCozinha.length; i++) {
+			tiposFormatados += tiposCozinha[i];
+			if (i < tiposCozinha.length - 1)
+				tiposFormatados += ",";
+		}
+
+		String precoStr = "";
+		for (int i = 0; i < faixaPreco; i++)
+			precoStr += "$";
+
+		return String.format("[%d ## %s ## %s ## %d ## %s ## [%s] ## %s ## %s-%s ## %s ## %b]", id, nome, cidade, capacidade, String.valueOf(avaliacao), tiposFormatados, precoStr, horarioAbertura.formatar(), horarioFechamento.formatar(), dataAbertura.formatar(), aberto);
+	}
+}
+
+class ColecaoRestaurantes {
+	private Restaurante[] restaurantes;
+	private int tamanho;
+
+	public void lerCsv(String path) throws IOException
+	{
+		restaurantes = new Restaurante[8192];
+		tamanho = 0;
+
+		try (Scanner sc = new Scanner(new File(path))) {
+			sc.nextLine();
+			while (sc.hasNextLine()) {
+				String linha = sc.nextLine();
+				if (linha.length() > 0) {
+					restaurantes[tamanho++] = Restaurante.parseRestaurante(linha);
+				}
+			}
+		}
+	}
+
+	public int getTamanho()
+	{
+		return tamanho;
+	}
+	public Restaurante[] getRestaurantes()
+	{
+		return restaurantes;
+	}
+}
+
+class Data {
+	private int dia;
+	private int mes;
+	private int ano;
+
+	public Data(int dia, int mes, int ano)
+	{
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	public static Data parseData(String s)
+	{
+		Scanner sc = new Scanner(s);
+		sc.useDelimiter("-");
+		int ano = sc.nextInt();
+		int mes = sc.nextInt();
+		int dia = sc.nextInt();
+		sc.close();
+		return new Data(dia, mes, ano);
+	}
+
+	public String formatar()
+	{
+		return String.format("%02d/%02d/%d", dia, mes, ano);
+	}
+}
+
+class Hora {
+	private int hora;
+	private int minuto;
+
+	public Hora(int hora, int minuto)
+	{
+		this.hora = hora;
+		this.minuto = minuto;
+	}
+
+	public static Hora parseHora(String s)
+	{
+		Scanner sc = new Scanner(s);
+		sc.useDelimiter("[^0-9]+");
+		int hora = sc.nextInt();
+		int minuto = sc.nextInt();
+		sc.close();
+		return new Hora(hora, minuto);
+	}
+
+	public String formatar()
+	{
+		return String.format("%02d:%02d", hora, minuto);
+	}
+}
+
+public class OrdenacaoInsercao {
+	// Variáveis para o Log
+	static int comparacoes = 0;
+	static int movimentacoes = 0;
+
+	public static void main(String[] args) throws IOException
+	{
+		ColecaoRestaurantes colecao = new ColecaoRestaurantes();
+		colecao.lerCsv("/tmp/restaurantes.csv");
+
+		Scanner sc = new Scanner(System.in);
+		Restaurante[] arrayBusca = new Restaurante[8192];
+		int n = 0;
+
+		while (sc.hasNext()) {
+			String entrada = sc.next();
+			if (entrada.compareTo("-1") != 0) {
+				int idBuscado = Integer.parseInt(entrada);
+				Restaurante[] todos = colecao.getRestaurantes();
+
+				for (int i = 0; i < colecao.getTamanho(); i++) {
+					if (todos[i].getId() == idBuscado) {
+						arrayBusca[n++] = todos[i];
+						//System.err.println(todos[i].getCidade());
+						i = colecao.getTamanho();
+					}
+				}
+			}
+		}
+		sc.close();
+
+		// Cronometra
+		double inicio = System.nanoTime();
+		ordenaInsercao(arrayBusca, n); // Roda ordenacao
+		double tempoExecucaoMs = (System.nanoTime() - inicio) / 1_000_000.0;
+
+		// Salva log
+		try (FileWriter writer = new FileWriter("859563_insercao.txt")) {
+			writer.write(String.format("859563\t%d\t%d\t%g", comparacoes, movimentacoes, tempoExecucaoMs));
+		}
+
+		for (int i = 0; i < n; i++)
+			System.out.println(arrayBusca[i].formatar());
+	}
+
+	static void ordenaInsercao(Restaurante[] vec, int n)
+	{
+		for (int i = 1; i < n; ++i) {
+			Restaurante temp = vec[i];
+			int j;
+
+			for (j = i - 1; j >= 0 && /* Conta comparacao */ (++OrdenacaoInsercao.comparacoes >= 0) && temp.getCidade().compareTo(vec[j].getCidade()) < 0; --j) {
+				vec[j + 1] = vec[j];
+				++OrdenacaoInsercao.movimentacoes; // Conta movimentacao
+			}
+
+			vec[j + 1] = temp;
+			++OrdenacaoInsercao.movimentacoes; // Conta movimentacao
+		}
+	}
+}
--- ordenacaoSelecao.c
+++ ordenacaoSelecao.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h> 
-#include <time.h>   
+#include <string.h>
+#include <time.h>
 
 #define MAX_RESTAURANTES 8192
 #define MAX_TIPOS 32
@@ -9,71 +9,93 @@
 #define MAX_CIDADE 128
 #define MAX_LINHA 1024
 
-int meu_strlen(const char *s) {
-    int len = 0;
-    while (s[len] != '\0') len++;
-    return len;
+/* Funcoe auxiliares de forma manual*/
+
+int meu_strlen(const char *s)
+{
+	int len = 0;
+	while (s[len] != '\0')
+		len++;
+	return len;
 }
 
-void meu_strncpy(char *dest, const char *src, int n) {
-    int i;
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
-    for ( ; i < n; i++) {
-        dest[i] = '\0';
-    }
+void meu_strncpy(char *dest, const char *src, int n)
+{
+	int i;
+	for (i = 0; i < n && src[i] != '\0'; i++) {
+		dest[i] = src[i];
+	}
+	for (; i < n; i++) {
+		dest[i] = '\0';
+	}
 }
 
-void meu_strcat(char *dest, const char *src) {
-    int d = 0, s = 0;
-    while (dest[d] != '\0') d++;
-    while (src[s] != '\0') {
-        dest[d++] = src[s++];
-    }
-    dest[d] = '\0';
+void meu_strcat(char *dest, const char *src)
+{
+	int d = 0, s = 0;
+	while (dest[d] != '\0')
+		d++;
+	while (src[s] != '\0') {
+		dest[d++] = src[s++];
+	}
+	dest[d] = '\0';
 }
 
-char* meu_strchr(const char *s, char c) {
-    while (*s != '\0') {
-        if (*s == c) return (char*)s;
-        s++;
-    }
-    if (c == '\0') return (char*)s;
-    return NULL;
+char *meu_strchr(const char *s, char c)
+{
+	while (*s != '\0') {
+		if (*s == c)
+			return (char *)s;
+		s++;
+	}
+	if (c == '\0')
+		return (char *)s;
+	return NULL;
 }
 
-char *meu_strtok_r(char *str, const char *delim, char **saveptr) {
-    if (str == NULL) str = *saveptr;
-    if (*str == '\0') { *saveptr = str; return NULL; }
+char *meu_strtok_r(char *str, const char *delim, char **saveptr)
+{
+	if (str == NULL)
+		str = *saveptr;
+	if (*str == '\0') {
+		*saveptr = str;
+		return NULL;
+	}
 
-    while (*str != '\0') {
-        int is_delim = 0;
-        for (int i = 0; delim[i] != '\0'; i++) {
-            if (*str == delim[i]) is_delim = 1;
-        }
-        if (!is_delim) break;
-        str++;
-    }
-    if (*str == '\0') { *saveptr = str; return NULL; }
+	while (*str != '\0') {
+		int is_delim = 0;
+		for (int i = 0; delim[i] != '\0'; i++) {
+			if (*str == delim[i])
+				is_delim = 1;
+		}
+		if (!is_delim)
+			break;
+		str++;
+	}
+	if (*str == '\0') {
+		*saveptr = str;
+		return NULL;
+	}
 
-    char *token = str;
-    while (*str != '\0') {
-        int is_delim = 0;
-        for (int i = 0; delim[i] != '\0'; i++) {
-            if (*str == delim[i]) is_delim = 1;
-        }
-        if (is_delim) break;
-        str++;
-    }
+	char *token = str;
+	while (*str != '\0') {
+		int is_delim = 0;
+		for (int i = 0; delim[i] != '\0'; i++) {
+			if (*str == delim[i])
+				is_delim = 1;
+		}
+		if (is_delim)
+			break;
+		str++;
+	}
 
-    if (*str != '\0') {
-        *str = '\0';
-        *saveptr = str + 1;
-    } else {
-        *saveptr = str;
-    }
-    return token;
+	if (*str != '\0') {
+		*str = '\0';
+		*saveptr = str + 1;
+	} else {
+		*saveptr = str;
+	}
+	return token;
 }
 
 typedef struct {
@@ -99,7 +121,7 @@ typedef struct {
 	Hora horarioAbertura;
 	Hora horarioFechamento;
 	Data dataAbertura;
-	int aberto; 
+	int aberto;
 } Restaurante;
 
 typedef struct {
@@ -107,7 +129,10 @@ typedef struct {
 	int tamanho;
 } ColecaoRestaurantes;
 
-Hora parseHora(const char *s) {
+/* Hora */
+
+Hora parseHora(const char *s)
+{
 	Hora h = { 0, 0 };
 	int hh, mm;
 	if (sscanf(s, "%d:%d", &hh, &mm) == 2) {
@@ -117,12 +142,16 @@ Hora parseHora(const char *s) {
 	return h;
 }
 
-char *formatarHora(const Hora *h, char *buf) {
+char *formatarHora(const Hora *h, char *buf)
+{
 	sprintf(buf, "%02d:%02d", h->hora, h->minuto);
 	return buf;
 }
 
-Data parseData(const char *s) {
+/* Data */
+
+Data parseData(const char *s)
+{
 	Data d = { 0, 0, 0 };
 	int ano, mes, dia;
 	if (sscanf(s, "%d-%d-%d", &ano, &mes, &dia) == 3) {
@@ -133,18 +162,20 @@ Data parseData(const char *s) {
 	return d;
 }
 
-char *formatarData(const Data *d, char *buf) {
+char *formatarData(const Data *d, char *buf)
+{
 	sprintf(buf, "%02d/%02d/%d", d->dia, d->mes, d->ano);
 	return buf;
 }
 
-static void parseTiposCozinha(const char *s, char tipos[][MAX_NOME], int *numTipos) {
+static void parseTiposCozinha(const char *s, char tipos[][MAX_NOME], int *numTipos)
+{
 	*numTipos = 0;
 	char temp[MAX_LINHA];
 	meu_strncpy(temp, s, sizeof(temp) - 1);
 	temp[sizeof(temp) - 1] = '\0';
 
-    char *saveptr_tipos;
+	char *saveptr_tipos;
 	char *token = meu_strtok_r(temp, ";", &saveptr_tipos);
 	while (token != NULL && *numTipos < MAX_TIPOS) {
 		meu_strncpy(tipos[*numTipos], token, MAX_NOME - 1);
@@ -154,7 +185,10 @@ static void parseTiposCozinha(const char *s, char tipos[][MAX_NOME], int *numTip
 	}
 }
 
-Restaurante parseRestaurante(const char *s) {
+/* Restaurante */
+
+Restaurante parseRestaurante(const char *s)
+{
 	Restaurante r = { 0 };
 	char linha[MAX_LINHA];
 	meu_strncpy(linha, s, sizeof(linha) - 1);
@@ -169,7 +203,9 @@ Restaurante parseRestaurante(const char *s) {
 
 	while (token != NULL) {
 		switch (campo) {
-		case 0: r.id = atoi(token); break;
+		case 0:
+			r.id = atoi(token);
+			break;
 		case 1:
 			meu_strncpy(r.nome, token, MAX_NOME - 1);
 			r.nome[MAX_NOME - 1] = '\0';
@@ -178,40 +214,56 @@ Restaurante parseRestaurante(const char *s) {
 			meu_strncpy(r.cidade, token, MAX_CIDADE - 1);
 			r.cidade[MAX_CIDADE - 1] = '\0';
 			break;
-		case 3: r.capacidade = atoi(token); break;
-		case 4: r.avaliacao = atof(token); break;
+		case 3:
+			r.capacidade = atoi(token);
+			break;
+		case 4:
+			r.avaliacao = atof(token);
+			break;
 		case 5:
 			meu_strncpy(tiposStr, token, sizeof(tiposStr) - 1);
 			tiposStr[sizeof(tiposStr) - 1] = '\0';
 			parseTiposCozinha(tiposStr, r.tiposCozinha, &r.numTipos);
 			break;
-		case 6: r.faixaPreco = meu_strlen(token); break;
-		case 7: {
+		case 6:
+			r.faixaPreco = meu_strlen(token);
+			break;
+		case 7: // horário "HH:MM-HH:MM"
+		{
 			char horA[16] = { 0 }, horF[16] = { 0 };
 			sscanf(token, "%15[^-]", horA);
 			char *dash = meu_strchr(token, '-');
-			if (dash) sscanf(dash + 1, "%15[^,]", horF);
+			if (dash)
+				sscanf(dash + 1, "%15[^,]", horF);
 			r.horarioAbertura = parseHora(horA);
 			r.horarioFechamento = parseHora(horF);
 		} break;
-		case 8: r.dataAbertura = parseData(token); break;
-		case 9: r.aberto = (strcmp(token, "true") == 0) ? 1 : 0; break;
+		case 8: // data
+			r.dataAbertura = parseData(token);
+			break;
+		case 9: // aberto
+			r.aberto = (strcmp(token, "true") == 0) ? 1 : 0;
+			break;
 		}
 		token = meu_strtok_r(NULL, ",", &saveptr);
 		campo++;
 	}
+
 	return r;
 }
 
-char *formatarRestaurante(const Restaurante *r, char *buf) {
+char *formatarRestaurante(const Restaurante *r, char *buf)
+{
 	char tiposFormatados[MAX_LINHA] = "";
 	for (int i = 0; i < r->numTipos; i++) {
 		meu_strcat(tiposFormatados, r->tiposCozinha[i]);
-		if (i < r->numTipos - 1) meu_strcat(tiposFormatados, ",");
+		if (i < r->numTipos - 1)
+			meu_strcat(tiposFormatados, ",");
 	}
 
 	char precoStr[32] = "";
-	for (int i = 0; i < r->faixaPreco && i < 31; i++) precoStr[i] = '$';
+	for (int i = 0; i < r->faixaPreco && i < 31; i++)
+		precoStr[i] = '$';
 	precoStr[r->faixaPreco < 31 ? r->faixaPreco : 31] = '\0';
 
 	char hA[16], hF[16], dStr[32];
@@ -219,98 +271,110 @@ char *formatarRestaurante(const Restaurante *r, char *buf) {
 	formatarHora(&r->horarioFechamento, hF);
 	formatarData(&r->dataAbertura, dStr);
 
-	sprintf(buf, "[%d ## %s ## %s ## %d ## %.1f ## [%s] ## %s ## %s-%s ## %s ## %s]", 
-            r->id, r->nome, r->cidade, r->capacidade, r->avaliacao, tiposFormatados, precoStr, hA, hF, dStr,
+	sprintf(buf, "[%d ## %s ## %s ## %d ## %.1f ## [%s] ## %s ## %s-%s ## %s ## %s]", r->id, r->nome, r->cidade, r->capacidade, r->avaliacao, tiposFormatados, precoStr, hA, hF, dStr,
 			r->aberto ? "true" : "false");
 
 	return buf;
 }
 
-void lerCsv(ColecaoRestaurantes *colecao, const char *path) {
+/* ColecaoRestaurantes */
+
+void lerCsv(ColecaoRestaurantes *colecao, const char *path)
+{
 	colecao->tamanho = 0;
 	FILE *file = fopen(path, "r");
-	if (!file) return;
 
 	char linha[MAX_LINHA];
+	/* pular cabecalho */
 	if (fgets(linha, sizeof(linha), file) == NULL) {
 		fclose(file);
 		return;
 	}
 
 	while (fgets(linha, sizeof(linha), file) && colecao->tamanho < MAX_RESTAURANTES) {
-        for (int i = 0; linha[i] != '\0'; i++) {
-            if (linha[i] == '\n' || linha[i] == '\r') {
-                linha[i] = '\0';
-                break;
-            }
-        }
+		/* remover newline manualmente */
+		for (int i = 0; linha[i] != '\0'; i++) {
+			if (linha[i] == '\n' || linha[i] == '\r') {
+				linha[i] = '\0';
+				break;
+			}
+		}
+
 		if (meu_strlen(linha) > 0)
 			colecao->restaurantes[colecao->tamanho++] = parseRestaurante(linha);
 	}
+
 	fclose(file);
 }
 
+/* ========================================================================== */
 
+// Contadores
+static int num_comparacoes;
+static int num_movimentacoes;
 
-int main() {
-	ColecaoRestaurantes *colecao = malloc(sizeof(*colecao));
-	lerCsv(colecao, "restaurantes.csv");
+static int compara(Restaurante a, Restaurante b)
+{
+	return strcmp(a.nome, b.nome);
+}
 
-	Restaurante arrayBusca[MAX_RESTAURANTES];
-	int n = 0;
-	char input[32];
+static void swap(Restaurante *a, Restaurante *b) {
+	Restaurante tmp = *a;
+	*a = *b;
+	*b = tmp;
+	++num_movimentacoes; // Conta movimentacao
+}
 
-	//Le a entrada ate encontrar fim
-	while (scanf("%s", input) == 1) {
-        if (strcmp(input, "FIM") == 0) break;
-        
-        int idBuscado = atoi(input);
+static void selecao(ColecaoRestaurantes *c)
+{
+	for (int i = 0; i < c->tamanho; ++i) {
+		int menor = i;
+
+		for (int j = i + 1; j < c->tamanho; ++j) {
+			++num_comparacoes; // Conta comparacao
+			if (compara(c->restaurantes[j], c->restaurantes[menor]) < 0)
+				menor = j;
+		}
+
+		swap(&c->restaurantes[i], &c->restaurantes[menor]);
+	}
+}
+
+int main(void)
+{
+	ColecaoRestaurantes *colecao = calloc(1, sizeof(*colecao));
+	ColecaoRestaurantes *selecionados = calloc(1, sizeof(*selecionados));
+	lerCsv(colecao, "/tmp/restaurantes.csv");
+
+	int idBuscado;
+	char buf[2048];
+
+	while (scanf("%d", &idBuscado) == 1) {
 		for (int i = 0; i < colecao->tamanho; i++) {
 			if (colecao->restaurantes[i].id == idBuscado) {
-                arrayBusca[n++] = colecao->restaurantes[i];
-				break;
+				selecionados->restaurantes[selecionados->tamanho++] = colecao->restaurantes[i];
+				i = MAX_RESTAURANTES;
 			}
 		}
 	}
 
-    // Variáveis para o arquivo de log
-    int comparacoes = 0;
-    int movimentacoes = 0;
-    clock_t inicio = clock();
+	// Cronometra
+	struct timespec time_begin, time_end;
+	clock_gettime(CLOCK_MONOTONIC, &time_begin);
+	selecao(selecionados); // Roda ordenacao
+	clock_gettime(CLOCK_MONOTONIC, &time_end);
+	double duracao_segundos = time_end.tv_sec - time_begin.tv_sec + ((double)(time_end.tv_nsec - time_begin.tv_sec))/1000000000;
 
-    // Inicio da ordenacao por selecao
-    for (int i = 0; i < n - 1; i++) {
-        int menor = i;
-        for (int j = i + 1; j < n; j++) {
-            comparacoes++;
-            if (strcmp(arrayBusca[j].nome, arrayBusca[menor].nome) < 0) {
-                menor = j;
-            }
-        }
-        
-        // Se menor diferente, troca a posicao
-        if (menor != i) {
-            Restaurante temp = arrayBusca[i];
-            arrayBusca[i] = arrayBusca[menor];
-            arrayBusca[menor] = temp;
-            movimentacoes += 3;
-        }
-    }
+	// Log
+	FILE *log_file = fopen("859563_selecao.txt", "w");
+	fprintf(log_file, "%s\t%d\t%d\t%g", "859563", num_comparacoes, num_movimentacoes, duracao_segundos);
+	fclose(log_file);
 
-    // Tempo para a finalizacao 
-    clock_t fim = clock();
-    double tempo = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
-    FILE *log = fopen("matricula_selecao.txt", "w");
-    if (log) {
-        
-        fprintf(log, "859563\t%d\t%d\t%f\n", comparacoes, movimentacoes, tempo);
-        fclose(log);
-    }
-	char buf[2048];
-    for (int i = 0; i < n; i++) {
-        printf("%s\n", formatarRestaurante(&arrayBusca[i], buf));
-    }
+	char printBuf[512];
+	for (int i = 0; i < selecionados->tamanho; ++i)
+		puts(formatarRestaurante(&selecionados->restaurantes[i], printBuf));
 
+	free(selecionados);
 	free(colecao);
 	return 0;
 }
